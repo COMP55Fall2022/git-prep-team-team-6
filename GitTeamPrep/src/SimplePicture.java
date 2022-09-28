bb43752ccbc6401346f47a83b72dd7de813bfc44
ArsalanKhojazada <a_khojazada@u.pacific.edu>
2022-09-28 16:23:53 -0700

    I changed the image to a constant.

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -3,14 +3,16 @@ import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 public class SimplePicture extends GraphicsProgram {
+	private static final String ROBOT_PNG = "robot.png";
 	public static int SIZE = 700;
+	
 	
 	public void init() {
 		setSize(SIZE, SIZE);
 	}
 	
 	public void run() {
-		GImage robot = new GImage("robot.png", 200, 200);
+		GImage robot = new GImage(ROBOT_PNG, 200, 200);
 		add(robot);
 		GLabel label = new GLabel("How I feel when using github hackslash", 200, 400);
 		add(label);
